38d402930be0159147b78cb6466a0f826f1fbe27
evelyn <eli_home@outlook.com>
2017-05-13 01:13:26 +0800

    add function.md in docs

--- hello.py
+++ hello.py
@@ -3,7 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Welcome To Gooseberry!"
+
+@app.route("/index/")
+def index():
+    return render_template("index.html")
+
+@app.route("/user/")
+def user():
+    return render_template("user.html")
+
+@app.route("/invitation/<inv_id>")
+def invitation(inv_id):
+    if(True):
+        return render_template("invitation.html")
+    else:
+        return "Invitation does not exist!"
 
 if __name__ == "__main__":
     app.run()

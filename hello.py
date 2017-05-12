5a440a318bfe0410219da3b39ff7f33bc5710bb5
evelyn <eli_home@outlook.com>
2017-05-13 02:41:31 +0800

    temporal

--- hello.py
+++ hello.py
@@ -1,24 +1,37 @@
 from flask import Flask
+from flask import abort, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome To Gooseberry!"
+    return render_template("hello.html")
 
 @app.route("/index/")
 def index():
-    return render_template("index.html")
+    if not signed-in:
+        return redirect(url_for('hello'))
+    else:
+        get invitation list
+        get join list
+        get 
+        return render_template("index.html")
 
 @app.route("/user/")
 def user():
-    return render_template("user.html")
+    if not signed-in:
+        return redirect(url_for('hello'))
+    else:
+        return render_template("user.html")
 
 @app.route("/invitation/<inv_id>")
 def invitation(inv_id):
-    if(True):
-        return render_template("invitation.html")
+    if not signed-in:
+        return redirect(url_for('hello'))
     else:
-        return "Invitation does not exist!"
+        if id valid:
+            return render_template("invitation.html")
+        else:
+            return "Invitation does not exist!"
 
 if __name__ == "__main__":
     app.run()

c5d94c9ffb3b3a8106337b714c1a73a5f23d5102
evelyn <eli_home@outlook.com>
2017-05-29 21:14:35 +0800

    temp

--- static/script/index.js
+++ static/script/index.js
@@ -1,13 +1,10 @@
 function getInv(){
-  console.log("getInv");
-  alert("hey");
-  // $.ajax({
-  //   type: "POST",
-  //   url: "/getinv/",
-  //   data: {{uname}},
-  //   success: function(data) {
-  //     $(tbody#inv).append("<tr><td><a href=\"{{url_for('invitation',inv_id='001')}}\"> invitation 001 </a></td><td>voting</td><td>evelyn</td></tr>");
-  //   }
-  // });
+  $.ajax({
+    type: "POST",
+    url: "/getinv/",
+    success: function(data) {
+      $(#invTbody).append("<tr><td><a href=\"{{url_for('invitation',inv_id='001')}}\"> invitation 001 </a></td><td>voting</td><td>evelyn</td></tr>");
+    }
+  });
   return true;
 }
